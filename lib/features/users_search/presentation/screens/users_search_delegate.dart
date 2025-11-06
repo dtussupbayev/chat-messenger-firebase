3168e1d4478b0826d805b7398946c4fd6e357826
Daulet Tussupbay <daulet.tusupbaev@gmail.com>
2025-11-06 11:26:52 +0500

    feat: implement Users Search BLoC

---
+++ lib/features/users_search/presentation/screens/users_search_delegate.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:realtime_chat_app/features/chat/presentation/screens/chat_screen.dart';
+import 'package:realtime_chat_app/features/users_search/presentation/bloc/users_search_bloc.dart';
+import 'package:realtime_chat_app/generated/l10n.dart';
+
+class UsersSearchDelegate extends SearchDelegate<String> {
+  UsersSearchDelegate({required this.usersSearchBloc});
+
+  final UsersSearchBloc usersSearchBloc;
+
+  @override
+  String get searchFieldLabel => S.current.search;
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, '');
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return _buildSearchResults(context);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return _buildSearchResults(context);
+  }
+
+  Widget _buildSearchResults(BuildContext context) {
+    if (query.trim().isEmpty) {
+      return Center(child: Text(S.of(context).enter_search_query));
+    }
+    usersSearchBloc.add(UsersSearchEvent.searchUsers(query));
+    return BlocListener<UsersSearchBloc, UsersSearchState>(
+      bloc: usersSearchBloc,
+      listener: (context, state) {
+        switch (state) {
+          case ChatRoomCreated():
+            context.goNamed(
+              ChatScreen.routeName,
+              pathParameters: {'chatRoomId': state.chatRoomId},
+              queryParameters: {'firstName': state.user.firstName, 'lastName': state.user.lastName},
+            );
+          case Error():
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.message)));
+          default:
+        }
+      },
+      child: BlocBuilder<UsersSearchBloc, UsersSearchState>(
+        bloc: usersSearchBloc,
+        builder: (context, state) {
+          switch (state) {
+            case Initial():
+              return Container();
+            case Loading():
+              return const Center(child: CupertinoActivityIndicator());
+            case Success():
+              final users = state.users;
+              if (users.isEmpty) {
+                return Center(child: Text(S.current.noUsersFound));
+              }
+              return ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final user = users[index];
+                  return ListTile(
+                    leading: CircleAvatar(child: Text(user.firstLetters)),
+                    title: Text(user.fullName),
+                    subtitle: Text(user.email),
+                    onTap: () {
+                      usersSearchBloc.add(UsersSearchEvent.createChatRoom(user));
+                    },
+                  );
+                },
+              );
+            case Error():
+              return Center(child: Text('${S.current.error}: ${state.message}'));
+            case ChatRoomCreated():
+              return const Center(child: CupertinoActivityIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
