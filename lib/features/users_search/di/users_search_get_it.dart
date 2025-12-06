9051af590316c64a95c74921bfc405baf78b4ca5
Daulet Tussupbay <daulet.tusupbaev@gmail.com>
2025-12-06 15:56:21 +0500

    refactor(users_search): remove unused use cases
    Signed-off-by: Daulet Tussupbay <daulet.tusupbaev@gmail.com>

--- lib/features/users_search/di/users_search_get_it.dart
+++ lib/features/users_search/di/users_search_get_it.dart
@@ -2,8 +2,6 @@ import 'package:get_it/get_it.dart';
 import 'package:realtime_chat_app/features/users_search/data/datasources/users_search_remote_data_source.dart';
 import 'package:realtime_chat_app/features/users_search/data/repositories/users_search_repository_impl.dart';
 import 'package:realtime_chat_app/features/users_search/domain/repositories/users_search_repository.dart';
-import 'package:realtime_chat_app/features/users_search/domain/usecases/create_chat_room_usecase.dart';
-import 'package:realtime_chat_app/features/users_search/domain/usecases/search_users_usecase.dart';
 import 'package:realtime_chat_app/features/users_search/presentation/bloc/users_search_bloc.dart';
 
 void initUsersSearchDependencies(GetIt getIt) {
@@ -16,12 +14,6 @@ void initUsersSearchDependencies(GetIt getIt) {
         remoteDataSource: getIt.get<UsersSearchRemoteDataSource>(),
         auth: getIt.get(),
       ),
-    )
-    ..registerLazySingleton<CreateChatRoomUseCase>(
-      () => CreateChatRoomUseCase(repository: getIt.get<UsersSearchRepository>()),
-    )
-    ..registerLazySingleton<SearchUsersUseCase>(
-      () => SearchUsersUseCase(repository: getIt.get<UsersSearchRepository>()),
     )
     ..registerFactory<UsersSearchBloc>(
       () => UsersSearchBloc(
