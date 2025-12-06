93136fcd8cd7e605bae9782225e6430290699c45
Daulet Tussupbay <daulet.tusupbaev@gmail.com>
2025-12-06 17:37:24 +0500

    refactor(chat): Remove use cases and decompose ChatScreen
    Signed-off-by: Daulet Tussupbay <daulet.tusupbaev@gmail.com>

--- lib/features/chat/presentation/bloc/chat_bloc.dart
+++ lib/features/chat/presentation/bloc/chat_bloc.dart
@@ -3,28 +3,22 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:realtime_chat_app/features/chat/domain/entities/message_entity.dart';
-import 'package:realtime_chat_app/features/chat/domain/use_cases/delete_message_use_case.dart';
-import 'package:realtime_chat_app/features/chat/domain/use_cases/get_message_list_use_case.dart';
-import 'package:realtime_chat_app/features/chat/domain/use_cases/send_message_use_case.dart';
+import 'package:realtime_chat_app/features/chat/domain/repositories/chat_repository.dart';
 
+part 'chat_bloc.freezed.dart';
 part 'chat_event.dart';
 part 'chat_state.dart';
-part 'chat_bloc.freezed.dart';
 
 class ChatBloc extends Bloc<ChatEvent, ChatState> {
   ChatBloc({
-    required this.getMessageListUseCase,
-    required this.sendMessageUseCase,
-    required this.deleteMessageUseCase,
+    required this.chatRepository,
   }) : super(const ChatState()) {
     on<ChatSubscriptionRequested>(_onSubscriptionRequested);
     on<ChatMessageSended>(_onMessageSended);
     on<ChatMessageDeleted>(_onMessageDeleted);
   }
 
-  final GetMessageListUseCase getMessageListUseCase;
-  final SendMessageUseCase sendMessageUseCase;
-  final DeleteMessageUseCase deleteMessageUseCase;
+  final IChatRepository chatRepository;
 
   Future<void> _onSubscriptionRequested(
     ChatSubscriptionRequested event,
@@ -33,21 +27,23 @@ class ChatBloc extends Bloc<ChatEvent, ChatState> {
     emit(state.copyWith(status: ChatStatus.loading));
 
     await emit.forEach<List<MessageEntity>>(
-      getMessageListUseCase.execute(event.chatRoomId),
+      chatRepository.getMessageList(event.chatRoomId),
       onData: (messages) => state.copyWith(status: ChatStatus.success, messages: messages),
       onError: (_, _) => state.copyWith(status: ChatStatus.failure),
     );
   }
 
   Future<void> _onMessageSended(ChatMessageSended event, Emitter<ChatState> emit) async {
-    await sendMessageUseCase.execute(
-      SendMessageParams(message: event.message, chatRoomId: event.chatRoomId),
+    await chatRepository.sendMessage(
+      event.message,
+      event.chatRoomId,
     );
   }
 
   Future<void> _onMessageDeleted(ChatMessageDeleted event, Emitter<ChatState> emit) async {
-    await deleteMessageUseCase.execute(
-      DeleteMessageParams(message: event.message, chatRoomId: event.chatRoomId),
+    await chatRepository.deleteMessage(
+      event.message,
+      event.chatRoomId,
     );
   }
 }
